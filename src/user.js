101ea64a620a5c565c82c31f9c933c51b392af54
Ashwin Mishra <ashwinmishra272@gmail.com>
2024-03-17 20:53:50 +0530

    2player matching

--- src/user.js
+++ src/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 var clients = []
+pairs = {}
 
 app.use(function (req, res, next) {
   console.log('middleware');
@@ -13,25 +14,37 @@ app.get('/', function (req, res, next) {
   console.log('get route', req.testing);
   res.end();
 });
-// ws.clients.forEach(function each(client) {
-//   if (client !== ws && client.readyState === WebSocket.OPEN) {
-//     client.send(data, { binary: isBinary });
-//   }
-// });
+app.ws('/:id', function (ws, req) {
+  let pairId = req.params.id
+  console.log(pairId)
+  if (!pairId && pairId !== 0) {
+    console.log("1st if")
+    ws.close()
 
-app.ws('/', function (ws, req) {
-  clients.push(ws);
+  }
+  else if (pairs.hasOwnProperty(pairId) && pairs[pairId].length >= 2) {
+    console.log("1st esle if")
+    ws.close()
+
+  }
+  else if (!pairs.hasOwnProperty(pairId) || pairs[pairId].length == 1) {
+    if (!pairs.hasOwnProperty(pairId)) {
+      pairs[pairId] = []
+    }
+    pairs[pairId].push(ws)
+    console.log("2nd esle if")
+  }
   ws.on('message', function (msg) {
-    clients.forEach(function (client) {
-      if (client !== ws) {
-        client.send(`message receiving:: ${msg}`)
-      }
-    })
-    // ws.send(`message received:: ${msg}`)
-    console.log(msg);
-  });
-  console.log('socket', req.testing);
-  console.log("printing hello world");
-});
-
+    if (pairs[pairId].length === 1) {
+      ws.send("Waiting for your pair")
+    }
+    else {
+      pairs[pairId].forEach(function (client) {
+        if (client != ws) {
+          client.send(`Joining game ${msg}`)
+        }
+      })
+    }
+  })
+})
 app.listen(3000);
